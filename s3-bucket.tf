982f5a49da9999522d1739f7614b29d81e689204
Nick Chow <nicky.chow@gmail.com>
2021-10-13 05:10:04 +0000

    updated s3 module version

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,6 @@
 module "s3-bucket" {
   source  = "app.terraform.io/MarsDefense/s3-bucket/aws"
-  version = "2.9.0"
+  version = "2.2.0"
   # insert required variables here
 
   bucket_prefix = "infra-nc"
